d9f0a523d261bbfd0b651634bbf97ff4ec9d114a
Ava Marie Bullock <ava.m.bullock@gmail.com>
2021-06-18 19:31:28 -0600

    created query to query 'me' and return userdata

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -6,6 +6,19 @@ const { signToken } = require('../utils/auth');
 
 const resolvers = {
 
+    Query: {
+        me: async (parent, args, context) => {
+            if(context.user){
+                const userData = await User.findOne({ _id: context.user._id })
+                .select('-__v -password')
+                .populate('books')
+
+                return userData;
+            }
+            throw new AuthenticationError('You are not logged in.');
+        }
+    }
+
 
 }
 
